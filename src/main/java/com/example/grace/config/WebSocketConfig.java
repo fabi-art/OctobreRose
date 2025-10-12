413d204504aed848c28ea4f24778371abefa712f
Lauraine <fabienneouedra@gmail.com>
2025-10-12 19:40:44 +0000

    Ajout dependance Web Socket

--- src/main/java/com/example/grace/config/WebSocketConfig.java
+++ src/main/java/com/example/grace/config/WebSocketConfig.java
@@ -39,4 +39,3 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
-
--- src/main/java/com/example/grace/security/WebSecurityConfig.java
+++ src/main/java/com/example/grace/security/WebSecurityConfig.java
@@ -66,12 +66,10 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/admin/**").permitAll()
-<<<<<<< HEAD
+
                 .requestMatchers("/api/campagnes/**").permitAll()
                 .requestMatchers("/api/dons/campagne/**").permitAll()
-=======
 
->>>>>>> origin/dev-anita
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -82,12 +80,7 @@ public class WebSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> origin/dev-anita
 }
 
 
